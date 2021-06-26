7ba3d8db07cb32d1e3e65cd9a2e6ca38ac49c05b
rk_zhang <40221744+qmhc@users.noreply.github.com>
2021-06-26 09:19:08 +0800

    perf(progress): using transfrom to improve performance

---
+++ components/progress/__serve__/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <Progress :percentage="percentage"></Progress>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Progress from '../progress.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Progress
+  },
+  data() {
+    return {
+      percentage: 60
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.vxp-select {
+  width: 300px;
+}
+</style>
