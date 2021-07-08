b639c2eb13c62b00d72010cc4c30328d09b2835a
rk_zhang <40221744+qmhc@users.noreply.github.com>
2021-07-08 09:33:09 +0800

    feat(time-ago): add time-ago component

---
+++ components/time-ago/__serve__/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <TimeAgo datetime="2021-07-07 08:17:00" title></TimeAgo>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { TimeAgo } from '..'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    TimeAgo
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
---
+++ components/time-ago/time-ago.vue
@@ -0,0 +1,87 @@
+<template>
+  <span :class="prefix" :title="currentTitle">
+    {{ timeAgo }}
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed, watch, onBeforeUnmount } from 'vue'
+import { useConfiguredProps } from '@/common/config/install'
+import { toDate, format } from '@/common/utils/date'
+import { getId, subscribe, unsubscribe, computeTimeAgo } from './helper'
+
+import type { PropType } from 'vue'
+import type { Dateable } from '@/common/utils/date'
+
+const props = useConfiguredProps('timeAgo', {
+  datetime: {
+    type: [String, Number, Date] as PropType<Dateable>,
+    default: () => Date.now()
+  },
+  interval: {
+    type: Number,
+    default: 10,
+    validator: (value: number) => value >= 5
+  },
+  title: {
+    type: [Boolean, String],
+    default: false
+  },
+  titleFormat: {
+    type: String,
+    default: 'yyyy-MM-dd HH:mm:ss'
+  }
+})
+
+export default defineComponent({
+  name: 'TimeAgo',
+  props,
+  setup(props) {
+    const prefix = 'vxp-time-ago'
+    const datetime = toDate(props.datetime)
+    const timeAgo = ref(computeTimeAgo(datetime))
+
+    const id = getId()
+    const record = {
+      datetime,
+      timeAgo,
+      interval: props.interval * 1000,
+      updated: Date.now()
+    }
+
+    const currentTitle = computed(() => {
+      if (!props.title) return undefined
+
+      if (typeof props.title === 'string') return props.title
+
+      return format(props.datetime, props.titleFormat)
+    })
+
+    subscribe(id, record)
+
+    watch(
+      () => props.datetime,
+      value => {
+        record.datetime = toDate(value)
+      }
+    )
+    watch(
+      () => props.interval,
+      value => {
+        record.interval = value * 1000
+      }
+    )
+
+    onBeforeUnmount(() => {
+      unsubscribe(id)
+    })
+
+    return {
+      prefix,
+      timeAgo,
+
+      currentTitle
+    }
+  }
+})
+</script>
