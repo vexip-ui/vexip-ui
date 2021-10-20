dd465434a34907ae1c8b98420588598280e1a9bc
rk_zhang <40221744+qmhc@users.noreply.github.com>
2021-10-20 14:42:28 +0800

    chore(tooltip): add serve files

---
+++ components/tooltip/__serve__/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div style="padding: 60px;">
+    <Tooltip>
+      <Button> 气泡提示 </Button>
+      <template #tip>
+        一些提示的内容
+      </template>
+    </Tooltip>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Button } from '@/components/button'
+import { Tooltip } from '..'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Button,
+    Tooltip
+  },
+  data() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss"></style>
