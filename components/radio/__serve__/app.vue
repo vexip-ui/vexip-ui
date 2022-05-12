fdd2c8e03358d2261508ace4ff04bba82d636816
rk_zhang <40221744+qmhc@users.noreply.github.com>
2022-05-12 09:50:01 +0800

    wip: refactor radio style to use css vars

--- components/radio/__serve__/app.vue
+++ components/radio/__serve__/app.vue
@@ -7,26 +7,84 @@
   <p style="margin-top: 10px;">
     value: {{ value }}
   </p>
+  <template v-for="state in states" :key="state">
+    <RadioGroup :value="items[0]">
+      <Radio
+        v-for="(item, index) in items"
+        :key="item"
+        :state="state"
+        :label="item"
+        :disabled="index === 2 || index === 3"
+      ></Radio>
+    </RadioGroup>
+    <br />
+  </template>
+  <br />
+  <template v-for="state in states" :key="state">
+    <RadioGroup :value="items[3]">
+      <Radio
+        v-for="(item, index) in items"
+        :key="item"
+        :state="state"
+        :label="item"
+        :disabled="index === 2 || index === 3"
+      ></Radio>
+    </RadioGroup>
+    <br />
+  </template>
+  <br />
+  <template v-for="state in states" :key="state">
+    <RadioGroup :value="items[0]">
+      <Radio
+        v-for="(item, index) in items"
+        :key="item"
+        border
+        :state="state"
+        :label="item"
+        :disabled="index === 2 || index === 3"
+      ></Radio>
+    </RadioGroup>
+    <br />
+    <br />
+  </template>
+  <br />
+  <template v-for="size in sizes" :key="size">
+    <RadioGroup :value="items[0]">
+      <Radio
+        v-for="(item, index) in items"
+        :key="item"
+        :label="item"
+        :size="size"
+        :disabled="index === 2 || index === 3"
+      ></Radio>
+    </RadioGroup>
+    <br />
+  </template>
+  <br />
+  <template v-for="size in sizes" :key="size">
+    <RadioGroup :value="items[0]">
+      <Radio
+        v-for="(item, index) in items"
+        :key="item"
+        border
+        :label="item"
+        :size="size"
+        :disabled="index === 2 || index === 3"
+      ></Radio>
+    </RadioGroup>
+    <br />
+    <br />
+  </template>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import { ref } from 'vue'
 import Radio from '../radio.vue'
 import RadioGroup from '../radio-group.vue'
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    Radio,
-    RadioGroup
-  },
-  data() {
-    return {
-      value: '上海',
-      items: ['北京', '上海', '广州', '深圳']
-    }
-  }
-})
-</script>
+const value = ref('上海')
+const items = ['北京', '上海', '广州', '深圳']
 
-<style lang="scss"></style>
+const states = ['default', 'success', 'error', 'warning'] as const
+const sizes = ['small', 'default', 'large'] as const
+</script>
