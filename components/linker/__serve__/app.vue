3a0cbbc0fdee9948ddc415c025573b03eadf61e6
rk_zhang <40221744+qmhc@users.noreply.github.com>
2022-05-14 09:37:46 +0800

    wip: refactor linker style to use css vars

---
+++ components/linker/__serve__/app.vue
@@ -0,0 +1,36 @@
+<template>
+  <template v-for="(type) in types" :key="type">
+    <Linker :type="type">
+      链接
+    </Linker>
+  </template>
+  <br />
+  <br />
+  <template v-for="(type) in types" :key="type">
+    <Linker :type="type" disabled>
+      链接
+    </Linker>
+  </template>
+  <br />
+  <br />
+  <template v-for="(type) in types" :key="type">
+    <Linker :type="type" underline icon="user">
+      链接
+    </Linker>
+  </template>
+  <br />
+  <br />
+  <template v-for="(type) in types" :key="type">
+    <Linker :type="type" disabled icon="user">
+      链接
+    </Linker>
+  </template>
+</template>
+
+<script setup lang="ts">
+import { Linker } from '..'
+
+import '@/common/icons/user'
+
+const types = ['default', 'primary', 'success', 'error', 'warning', 'info'] as const
+</script>
