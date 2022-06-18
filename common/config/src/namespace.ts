89f5bbf2bfd73809380ee4fe7b5d9fef5915ae7f
qmhc <544022268@qq.com>
2022-06-18 20:38:38 +0800

    feat: add name helper

--- common/config/src/namespace.ts
+++ common/config/src/namespace.ts
@@ -27,3 +27,35 @@ export function configNamespace(sourceNamespace: string | Ref<string>, app?: App
 export function useNamespace() {
   return inject(PROVIDED_NAMESPACE, globalNamespace)
 }
+
+export function useNameHelper(block: string, namespace: Ref<string> | string = useNamespace()) {
+  const b = () => `${unref(namespace)}-${block}`
+  const be = (element: string) => `${b()}__${element}`
+  const bm = (modifier: string | number) => `${b()}--${modifier}`
+  const bem = (element: string, modifier: string | number) => `${b()}__${element}--${modifier}`
+  const bs = (suffix: string) => `${b()}-${suffix}`
+  const ns = (suffix: string) => `${unref(namespace)}-${suffix}`
+
+  const cv = (name: string) => `--${b()}-${name}`
+  const cvm = (map: Record<string, string>, style: Record<string, string> = {}) => {
+    Object.keys(map).forEach(name => {
+      style[cv(name)] = map[name]
+    })
+
+    return style
+  }
+
+  return {
+    b,
+    be,
+    bm,
+    bem,
+    bs,
+    ns,
+
+    cv,
+    cvm
+  }
+}
+
+export type NameHelper = ReturnType<typeof useNameHelper>
