496494593b52ec9ed4b6cc04001ee1aa2327613b
qmhc <40221744+qmhc@users.noreply.github.com>
2022-06-25 13:39:47 +0800

    docs: add demo

---
+++ docs/demos/viewer/toolbar/demo.zh-CN.vue
@@ -0,0 +1,57 @@
+<template>
+  <Space style="width: 900px;">
+    <div v-for="placement in placements.slice(0, 6)" :key="placement">
+      <div style="width: 400px;">
+        {{ placement }}
+      </div>
+      <Viewer
+        :toolbar-placement="placement"
+        width="400"
+        height="100"
+      >
+        <div class="content"></div>
+      </Viewer>
+    </div>
+  </Space>
+  <Space style="width: 900px; margin-top: 10px;">
+    <div v-for="placement in placements.slice(6)" :key="placement">
+      <div style="width: 100px;">
+        {{ placement }}
+      </div>
+      <Viewer
+        :toolbar-placement="placement"
+        width="100"
+        height="400"
+      >
+        <div class="content"></div>
+      </Viewer>
+    </div>
+  </Space>
+</template>
+
+<script setup lang="ts">
+import type { ToolbarPlacement } from 'vexip-ui/es/components/viewer'
+
+const placements: ToolbarPlacement[] = [
+  'top',
+  'bottom',
+  'top-start',
+  'bottom-start',
+  'top-end',
+  'bottom-end',
+  'left',
+  'left-start',
+  'left-end',
+  'right',
+  'right-start',
+  'right-end'
+]
+</script>
+
+<style scoped>
+.content {
+  width: 400px;
+  height: 400px;
+  background-color: var(--vxp-color-primary-base);
+}
+</style>
