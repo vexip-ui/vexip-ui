82b90f26a893df43b1bb607dc94b753a36b74b36
qmhc <544022268@qq.com>
2022-06-29 23:59:18 +0800

    feat(config): add global z-index config (#62)
    * feat: add z-index config
    
    * fix: adjust and add creat config

--- common/config/src/index.ts
+++ common/config/src/index.ts
@@ -1,3 +1,4 @@
 export * from './locale'
 export * from './namespace'
 export * from './props'
+export * from './z-index'
---
+++ common/config/src/z-index.ts
@@ -0,0 +1,55 @@
+import { computed, provide, inject, unref } from 'vue'
+
+import type { App, ComputedRef, Ref } from 'vue'
+
+export const PROVIDED_Z_INDEX = '___vxp-provided-z-index'
+
+let counter = 0
+let initZIndex = 2000
+
+if (document) {
+  const rootStyle = getComputedStyle(document.documentElement)
+  const cssZIndex = parseFloat(rootStyle.getPropertyValue('--vxp-z-index-popup').trim())
+
+  if (!Number.isNaN(cssZIndex)) {
+    initZIndex = cssZIndex
+  }
+}
+
+const globalZIndex = computed(() => initZIndex)
+
+function getOrDefault(num: number, def: number) {
+  return !Number.isNaN(num) ? num : def
+}
+
+export function configZIndex(sourceZIndex: number | Ref<number>, app?: App) {
+  if (app) {
+    const zIndex = computed(() => {
+      const zIndex = unref(sourceZIndex)
+
+      return getOrDefault(zIndex, globalZIndex.value)
+    })
+
+    app.provide(PROVIDED_Z_INDEX, zIndex)
+  } else {
+    const upstreamZIndex = inject<ComputedRef<number> | null>(PROVIDED_Z_INDEX, null)
+    const zIndex = computed(() => {
+      const zIndex = unref(sourceZIndex)
+
+      return getOrDefault(
+        zIndex,
+        upstreamZIndex
+          ? getOrDefault(upstreamZIndex.value, globalZIndex.value)
+          : globalZIndex.value
+      )
+    })
+
+    provide(PROVIDED_Z_INDEX, zIndex)
+  }
+}
+
+export function useZIndex() {
+  const zIndex = inject<ComputedRef<number>>(PROVIDED_Z_INDEX, globalZIndex)
+
+  return computed(() => zIndex.value + counter++)
+}
