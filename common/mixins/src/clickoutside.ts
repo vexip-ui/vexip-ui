ebf4842eafab6175a929e0e2cb26cca88a6b2347
qmhc <40221744+qmhc@users.noreply.github.com>
2022-07-14 15:55:04 +0800

    fix: useClickoutside directly passing a callback

--- common/mixins/src/clickoutside.ts
+++ common/mixins/src/clickoutside.ts
@@ -1,14 +1,15 @@
-import { ref, onMounted, onBeforeUnmount, nextTick } from 'vue'
+import { ref, unref, watch, getCurrentScope, onScopeDispose } from 'vue'
 import {
   CLICK_TYPE,
+  noop,
   createEvent,
   getObservers,
   observe,
   disconnect,
   dispatchEvent
 } from '@vexip-ui/utils'
+import { useListener } from './listener'
 
-import type { Ref } from 'vue'
 import type { TransferNode } from '@vexip-ui/utils'
 
 export const CLICK_OUTSIDE = 'clickoutside'
@@ -34,20 +35,38 @@ document.addEventListener(CLICK_TYPE, (event: Event & { path?: TransferNode[] })
 /**
  * 创建一个可以添加 clickoutside 事件的元素 Ref
  *
- * @param wrapper 需要处理的元素 ref，可不传
+ * @param
+ * @param target 需要处理的元素 ref，可不传
  */
-export function useClickOutside(wrapper: Ref<HTMLElement | null> = ref(null)) {
-  onMounted(() => {
-    nextTick(() => {
-      if (wrapper.value) {
-        observe(wrapper.value as TransferNode, CLICK_OUTSIDE)
+export function useClickOutside(handler: () => void, target = ref<HTMLElement | null>(null)) {
+  let remove = noop
+
+  const stopWatch = watch(
+    () => unref(target),
+    el => {
+      remove()
+
+      if (!el) {
+        return
       }
-    })
-  })
 
-  onBeforeUnmount(() => {
-    disconnect(wrapper.value as TransferNode, CLICK_OUTSIDE)
-  })
+      observe(target.value as TransferNode, CLICK_OUTSIDE)
 
-  return wrapper
+      remove = () => {
+        disconnect(target.value as TransferNode, CLICK_OUTSIDE)
+        remove = noop
+      }
+    },
+    { immediate: true, flush: 'post' }
+  )
+
+  const stop = () => {
+    stopWatch()
+    remove()
+  }
+
+  getCurrentScope() && onScopeDispose(stop)
+  useListener(target, CLICK_OUTSIDE, handler)
+
+  return target
 }
