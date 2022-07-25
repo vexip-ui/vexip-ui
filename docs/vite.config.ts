16443eba06e2c93cf0ecbcbfcc7dd49842b89bcb
qmhc <40221744+qmhc@users.noreply.github.com>
2022-07-25 14:40:08 +0800

    build: update vue dependencies

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig(({ command }) => {
             ]
           : [])
       ],
-      dedupe: useServer ? ['../components'] : []
+      dedupe: useServer ? ['../components', 'vue'] : ['vue']
     },
     server: {
       port: 9000,
