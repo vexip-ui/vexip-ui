47a98a9e50a47cef908653b73682cc3f15981de6
qmhc <544022268@qq.com>
2022-08-05 23:18:34 +0800

    test: add tag tests

--- components/tooltip/tests/tooltip.spec.tsx
+++ components/tooltip/tests/tooltip.spec.tsx
@@ -1,15 +1,45 @@
-import { describe, it, expect } from 'vitest'
+import { afterEach, describe, it, expect, vi } from 'vitest'
+import { nextTick } from 'vue'
 import { mount } from '@vue/test-utils'
 import { Tooltip } from '..'
 
+vi.useFakeTimers()
+
+const TEXT = 'Text'
+
 describe('Tooltip', () => {
-  it('render', () => {
-    const wrapper = mount(Tooltip, {
+  let wrapper: ReturnType<typeof createTooltip> | null
+
+  function createTooltip() {
+    return mount(Tooltip, {
       slots: {
-        trigger: () => <span class={'trigger'}></span>
+        trigger: () => <span class={'trigger'}>{TEXT}</span>,
+        default: () => <span class={'tip'}>{TEXT}</span>
       }
     })
+  }
+
+  afterEach(() => {
+    if (wrapper) {
+      wrapper.unmount()
+      wrapper = null
+    }
+  })
+
+  it('render', () => {
+    wrapper = createTooltip()
 
     expect(wrapper.find('.trigger').exists()).toBe(true)
+  })
+
+  it('hover trigger', async () => {
+    wrapper = createTooltip()
+
+    await wrapper.find('.trigger').trigger('mouseenter')
+    vi.runAllTimers()
+    await nextTick()
+    expect(wrapper.find('.vxp-tooltip__popper').exists()).toBe(true)
+    expect(wrapper.find('.vxp-tooltip__popper').classes()).toContain('vxp-tooltip-vars')
+    expect(wrapper.find('.tip').exists()).toBe(true)
   })
 })
