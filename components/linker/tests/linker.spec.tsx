35e11d5e5e65536388c1813a829dad8e76c2c41c
qmhc <40221744+qmhc@users.noreply.github.com>
2022-08-08 16:05:28 +0800

    test: add linker tests

--- components/linker/tests/linker.spec.tsx
+++ components/linker/tests/linker.spec.tsx
@@ -7,5 +7,44 @@ describe('Linker', () => {
     const wrapper = mount(Linker)
 
     expect(wrapper.classes()).toContain('vxp-linker-vars')
+    expect(wrapper.attributes('target')).toEqual('_blank')
+  })
+
+  it('type', () => {
+    (['primary', 'success', 'error', 'warning', 'info'] as const).forEach(type => {
+      const wrapper = mount(() => <Linker type={type}></Linker>)
+
+      expect(wrapper.find('.vxp-linker').classes()).toContain(`vxp-linker--${type}`)
+    })
+  })
+
+  it('disabled', async () => {
+    const wrapper = mount(Linker)
+
+    expect(wrapper.classes()).not.toContain('vxp-linker--disabled')
+
+    await wrapper.setProps({ disabled: true })
+    expect(wrapper.classes()).toContain('vxp-linker--disabled')
+  })
+
+  it('underline', async () => {
+    const wrapper = mount(Linker)
+
+    expect(wrapper.classes()).not.toContain('vxp-linker--underline')
+
+    await wrapper.setProps({ underline: true })
+    expect(wrapper.classes()).toContain('vxp-linker--underline')
+  })
+
+  it('to address', () => {
+    const wrapper = mount(() => <Linker to={'test'}></Linker>)
+
+    expect(wrapper.find('.vxp-linker').attributes('href')).toEqual('test')
+  })
+
+  it('target', () => {
+    const wrapper = mount(() => <Linker target={'_self'}></Linker>)
+
+    expect(wrapper.find('.vxp-linker').attributes('target')).toEqual('_self')
   })
 })
