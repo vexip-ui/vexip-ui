5602c52472f08055154b590c6e60ff541ca9908b
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2022-08-20 14:55:10 +0530

    docs: improve font styling✔

--- docs/views/homepage.vue
+++ docs/views/homepage.vue
@@ -68,8 +68,10 @@ function toRepository() {
 
   &__description {
     margin-bottom: 1rem;
-    font-size: 1.4rem;
+    font-size: 1.3rem;
     font-weight: 300;
+    font-weight: 400;
+    line-height: 1.2;
     color: var(--vxp-content-color-secondary);
     text-align: center;
   }
