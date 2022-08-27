8cab9a848b57cd67a9d9813c264b56b4c0376fe3
jz <zz541843@163.com>
2022-08-27 22:07:23 +0800

    test: add skeleton tests

--- components/skeleton/tests/skeleton.spec.tsx
+++ components/skeleton/tests/skeleton.spec.tsx
@@ -2,10 +2,106 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import { Skeleton } from '..'
 
+const TEXT = 'TEXT'
 describe('Skeleton', () => {
   it('render', () => {
     const wrapper = mount(Skeleton)
 
     expect(wrapper.classes()).toContain('vxp-skeleton-vars')
+  })
+
+  it('block', async () => {
+    const wrapper = mount(Skeleton, {
+      props: {
+        block: true
+      }
+    })
+    expect(wrapper.classes()).toContain('vxp-skeleton--block')
+  })
+
+  it('image', async () => {
+    const wrapper = mount(Skeleton, {
+      props: {
+        image: true
+      }
+    })
+    expect(wrapper.classes()).toContain('vxp-skeleton--image')
+  })
+
+  it('circle', async () => {
+    const wrapper = mount(Skeleton, {
+      props: {
+        circle: true
+      }
+    })
+    expect(wrapper.classes()).toContain('vxp-skeleton--circle')
+  })
+
+  it('width', async () => {
+    const wrapper = mount(Skeleton, {
+      props: {
+        block: true,
+        width: 20
+      }
+    })
+
+    expect(wrapper.find('.vxp-skeleton').attributes('style')).toContain('width: 20px')
+  })
+
+  it('height', async () => {
+    const wrapper = mount(Skeleton, {
+      props: {
+        block: true,
+        height: 20
+      }
+    })
+
+    expect(wrapper.find('.vxp-skeleton').attributes('style')).toContain('height: 20px')
+  })
+
+  it('spread', async () => {
+    const wrapper = mount(Skeleton, {
+      props: {
+        block: true,
+        spread: 20
+      }
+    })
+
+    expect(wrapper.find('.vxp-skeleton').attributes('style')).toContain('spread: 20px')
+  })
+
+  it('tag', () => {
+    const wrapper = mount(Skeleton, {
+      props: {
+        tag: 'xxx'
+      }
+    })
+    expect(wrapper.find('.vxp-skeleton').element.tagName).toEqual('XXX')
+  })
+
+  it('repeat', () => {
+    const wrapper = mount(Skeleton, {
+      props: {
+        repeat: 2
+      }
+    })
+    expect(wrapper.findAll('.vxp-skeleton').length).toBe(2)
+  })
+
+  it('slot', async () => {
+    const wrapper = mount(Skeleton, {
+      props: {
+        loading: true
+      },
+      slots: {
+        default: () => <span class={'content'}>{TEXT}</span>
+      }
+    })
+
+    expect(wrapper.find('.content').exists()).toBe(false)
+    await wrapper.setProps({
+      loading: false
+    })
+    expect(wrapper.find('.content').exists()).toBe(true)
   })
 })
