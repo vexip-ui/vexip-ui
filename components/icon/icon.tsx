d11ce043ad1c6d87a820408ed75ae4d22ef54afd
qmhc <40221744+qmhc@users.noreply.github.com>
2022-09-14 18:19:15 +0800

    chore(icon): remove name prop

--- components/icon/icon.tsx
+++ components/icon/icon.tsx
@@ -1,6 +1,5 @@
-import { defineComponent, h, ref, computed } from 'vue'
+import { defineComponent, h, computed } from 'vue'
 import { useNameHelper, useProps } from '@vexip-ui/config'
-import { iconMap, register } from './register'
 
 import type { PropType, CSSProperties } from 'vue'
 
@@ -11,9 +10,7 @@ const inOutProp = {
 
 export default defineComponent({
   name: 'Icon',
-  register,
   props: {
-    name: String,
     icon: Object,
     scale: [Number, String] as PropType<number | string>,
     title: String,
@@ -24,23 +21,6 @@ export default defineComponent({
   },
   setup(_props, { attrs, slots }) {
     const props = useProps('icon', _props, {
-      name: {
-        default: null,
-        validator: (value: string) => {
-          console.warn("[vexip-ui:Icon] prop 'name' has been deprecated, use 'icon' instead it.")
-
-          if (value && iconMap.has(value)) {
-            return true
-          }
-
-          console.warn(
-            `[vexip-ui:Icon] prop 'name' is referring to an unregistered icon '${value}'.`
-          )
-
-          return false
-        },
-        static: true
-      },
       icon: {
         default: null,
         static: true
@@ -63,8 +43,6 @@ export default defineComponent({
     })
 
     const nh = useNameHelper('icon')
-    const childrenWidth = ref(0)
-    const childrenHeight = ref(0)
 
     const computedScale = computed(() => {
       return Number(props.scale) || 1
@@ -80,37 +58,8 @@ export default defineComponent({
         [nh.bm(`pulse-${pulse}`)]: props.pulse
       }
     })
-    const icon = computed(() => {
-      return props.name ? iconMap.get(props.name) : null
-    })
-    const ratio = computed(() => {
-      if (!icon.value) return 1
-
-      const { width, height } = icon.value
-
-      return Math.max(width, height) / 16
-    })
-    const width = computed(() => {
-      return (
-        childrenWidth.value ||
-        (icon.value && (icon.value.width / ratio.value) * computedScale.value) ||
-        0
-      )
-    })
-    const height = computed(() => {
-      return (
-        childrenHeight.value ||
-        (icon.value && (icon.value.height / ratio.value) * computedScale.value) ||
-        0
-      )
-    })
     const style = computed<CSSProperties>(() => {
       return computedScale.value === 1 ? {} : { fontSize: `${computedScale.value}em` }
-    })
-    const viewBox = computed(() => {
-      return icon.value
-        ? `0 0 ${icon.value.width} ${icon.value.height}`
-        : `0 0 ${width.value} ${height.value}`
     })
 
     return () => {
@@ -135,29 +84,6 @@ export default defineComponent({
         return (
           <i {...iAttrs}>
             <g>{h(props.icon)}</g>
-          </i>
-        )
-      }
-
-      if (props.name) {
-        const content = props.title ? [<title>{props.title}</title>] : []
-
-        return (
-          <i {...iAttrs}>
-            <g>
-              <svg viewBox={viewBox.value}>
-                {...content}
-                {icon.value
-                  ? icon.value.paths
-                    .map((path, index) => <path {...path} key={index}></path>)
-                    .concat(
-                      icon.value.polygons.map((polygon, index) => (
-                          <polygon {...polygon} key={index}></polygon>
-                      ))
-                    )
-                  : []}
-              </svg>
-            </g>
           </i>
         )
       }
--- components/icon/index.ts
+++ components/icon/index.ts
@@ -1,9 +1,2 @@
-import _Icon from './icon'
-import { register } from './register'
-
-const Icon = _Icon as typeof _Icon & {
-  register: typeof register
-}
-
-export { Icon, register }
+export { default as Icon } from './icon'
 export type { IconProps, IconMinorProps } from './symbol'
