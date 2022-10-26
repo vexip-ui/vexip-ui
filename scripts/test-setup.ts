28903117f8d0c2ac804d64c80ae3fe0f7fd1c205
qmhc <40221744+qmhc@users.noreply.github.com>
2022-10-26 19:03:04 +0800

    test: mock and stub IntersectionObserver

--- scripts/test-setup.ts
+++ scripts/test-setup.ts
@@ -2,6 +2,31 @@ import { config } from '@vue/test-utils'
 import { vi, beforeEach } from 'vitest'
 import { ResizeObserver } from '@juggle/resize-observer'
 
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
+class IntersctionObserver {
+  readonly root: Element | Document | null = null
+  readonly rootMargin = ''
+  readonly thresholds: ReadonlyArray<number> = Object.freeze([])
+
+  constructor(callback: IntersectionObserverCallback, options?: IntersectionObserverInit) {
+    if (!callback && options) {
+      //
+    }
+  }
+
+  disconnect() {}
+
+  observe(target: Element) {}
+
+  takeRecords() {
+    return []
+  }
+
+  unobserve(target: Element) {}
+}
+/* eslint-enable */
+
 config.global.stubs = {
   Transition: {
     inheritAttrs: false,
@@ -18,6 +43,7 @@ config.global.stubs = {
 }
 
 vi.stubGlobal('ResizeObserver', ResizeObserver)
+vi.stubGlobal('IntersctionObserver', IntersctionObserver)
 
 beforeEach(() => {
   if (typeof document !== 'undefined') {
