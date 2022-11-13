1f8d36074bf59c7de994851a100f94669b1d73a2
snowingfox <1503401882@qq.com>
2022-11-13 12:50:54 +0800

    feat: optimize test cases

--- components/full-screen/tests/fullScreen.spec.tsx
+++ components/full-screen/tests/fullScreen.spec.tsx
@@ -6,13 +6,10 @@ describe('FullScreen', () => {
   it('should keep exited state when init', () => {
     const wrapper = mount(() => <FullScreen></FullScreen>)
 
-    expect(wrapper.find('div').classes()).toMatchInlineSnapshot(`
-      [
-        "vxp-full-screen",
-        "vxp-full-screen-vars",
-      ]
-    `)
-    expect(wrapper.find('div').classes()).toStrictEqual(['vxp-full-screen', 'vxp-full-screen-vars'])
+    expect(wrapper.find('.vxp-full-screen').classes()).toStrictEqual([
+      'vxp-full-screen',
+      'vxp-full-screen-vars'
+    ])
   })
 
   it('should work', async () => {
@@ -32,10 +29,14 @@ describe('FullScreen', () => {
     ))
 
     await wrapper.find('.enter').trigger('click')
-    expect(document.body.firstElementChild!.className).toContain('vxp-full-screen--full')
-    await (document.querySelector('.exit')! as HTMLButtonElement).click()
 
-    expect(wrapper.find('div').classes()).not.toContain('vxp-full-screen--full')
+    expect(document.querySelector('.vxp-full-screen')).toBeTruthy()
+    expect(document.querySelector('.vxp-full-screen')!.className).toContain('vxp-full-screen--full')
+
+    expect(document.querySelector('.exit')).toBeTruthy()
+    await (document.querySelector('.exit') as HTMLButtonElement).click()
+
+    expect(wrapper.find('.vxp-full-screen').classes()).not.toContain('vxp-full-screen--full')
   })
 
   it('should work via exposed methods', async () => {
@@ -44,14 +45,16 @@ describe('FullScreen', () => {
     const { enter, exit } = wrapper.findComponent(FullScreen).vm
 
     await enter()
-    expect(wrapper.find('div').classes()).toContain('vxp-full-screen--full')
+    expect(wrapper.find('.vxp-full-screen').classes()).toContain('vxp-full-screen--full')
     await exit()
-    expect(wrapper.find('div').classes()).not.toContain('vxp-full-screen--full')
+    expect(wrapper.find('.vxp-full-screen').classes()).not.toContain('vxp-full-screen--full')
 
     await enter('browser')
-    expect(document.body.firstElementChild?.className).toContain('vxp-full-screen--full')
+    expect(document.querySelector('.vxp-full-screen')).toBeTruthy()
+    expect(document.querySelector('.vxp-full-screen')!.className).toContain('vxp-full-screen--full')
+
     await exit()
-    expect(wrapper.find('div').classes()).not.toContain('vxp-full-screen--full')
+    expect(wrapper.find('.vxp-full-screen').classes()).not.toContain('vxp-full-screen--full')
   })
 
   it('should work via toggle', async () => {
@@ -60,7 +63,7 @@ describe('FullScreen', () => {
     const { toggle } = wrapper.findComponent(FullScreen).vm
 
     await toggle()
-    expect(wrapper.find('div').classes()).toContain('vxp-full-screen--full')
+    expect(wrapper.find('.vxp-full-screen').classes()).toContain('vxp-full-screen--full')
     await toggle()
     expect(wrapper.classes()).not.toContain('vxp-full-screen--full')
   })
@@ -70,9 +73,11 @@ describe('FullScreen', () => {
 
     const { enter } = wrapper.findComponent(FullScreen).vm
 
-    expect(wrapper.find('div').attributes().style).eq(undefined)
+    expect(wrapper.find('.vxp-full-screen').attributes().style).eq(undefined)
     await enter('window', 1)
-    expect(wrapper.find('div').attributes().style).toContain('--vxp-full-screen-z-index: 1;')
+    expect(wrapper.find('.vxp-full-screen').attributes().style).toContain(
+      '--vxp-full-screen-z-index: 1;'
+    )
   })
 
   test('should switch to another enterted state when current state is enterted.', async () => {
@@ -81,8 +86,10 @@ describe('FullScreen', () => {
     const { toggle } = wrapper.findComponent(FullScreen).vm
 
     await toggle()
-    expect(wrapper.find('div').classes()).toContain('vxp-full-screen--full')
+    expect(wrapper.find('.vxp-full-screen').classes()).toContain('vxp-full-screen--full')
+
     await toggle('browser')
-    expect(document.body.firstElementChild!.className).toContain('vxp-full-screen--full')
+    expect(document.querySelector('.vxp-full-screen')).toBeTruthy()
+    expect(document.querySelector('.vxp-full-screen')!.className).toContain('vxp-full-screen--full')
   })
 })
