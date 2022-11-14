04404d9d0927a470a7495b5d245e76275283ec94
qmhc <40221744+qmhc@users.noreply.github.com>
2022-11-14 16:51:36 +0800

    style: improve eslint rules for components codes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -52,6 +52,31 @@ module.exports = {
   },
   overrides: [
     {
+      files: ['components/**/*.vue'],
+      rules: {
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          {
+            prefer: 'type-imports',
+            disallowTypeAnnotations: false
+          }
+        ],
+        'vue/component-tags-order': [
+          'error',
+          {
+            order: ['template', 'script']
+          }
+        ],
+        'vue/no-restricted-block': [
+          'error',
+          {
+            element: '/[^(template|script)]/',
+            message: 'Do not use blocks other than <template> or <script>.'
+          }
+        ]
+      }
+    },
+    {
       files: ['docs/**'],
       globals: {
         __ROLLBACK_LANG__: 'readonly'
