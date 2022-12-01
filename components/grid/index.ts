570b6f769320c77f670a994334b08303df17b951
qmhc <40221744+qmhc@users.noreply.github.com>
2022-12-01 09:14:28 +0800

    feat: export currentBreakPoint ref

--- components/grid/index.ts
+++ components/grid/index.ts
@@ -1,3 +1,4 @@
 export { default as Grid } from './grid'
+export { currentBreakPoint } from './helpler'
 export type { GridProps, GridCProps } from './props'
 export type { GridJustify, GridAlign, CellFlex } from './symbol'
