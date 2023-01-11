260001cb7d32402e835a1b1a5c2336816af9de90
qmhc <40221744+qmhc@users.noreply.github.com>
2023-01-11 10:29:44 +0800

    fix(form): icon and loading slots for submit and reset not effective

--- components/form/form-submit.vue
+++ components/form/form-submit.vue
@@ -23,6 +23,12 @@
     <slot>
       {{ props.label || locale.submit }}
     </slot>
+    <template v-if="$slots.icon" #icon>
+      <slot name="icon"></slot>
+    </template>
+    <template v-if="$slots.loading" #loading>
+      <slot name="loading"></slot>
+    </template>
     <button
       v-if="isNative"
       ref="submit"
---
+++ docs/demos/form/manual/demo.zh-CN.vue
@@ -0,0 +1,54 @@
+<template>
+  <ConfigProvider :props="providedProps">
+    <Form ref="form" :model="formModel" style="max-width: 500px;">
+      <FormItem label="Input" prop="input">
+        <Input></Input>
+      </FormItem>
+      <FormItem label="Select" prop="select">
+        <Select :options="options"></Select>
+      </FormItem>
+      <FormItem label="Date" prop="date">
+        <DatePicker></DatePicker>
+      </FormItem>
+      <FormItem action>
+        <Button type="warning" :loading="loading" @click="handleValidate">
+          验证
+        </Button>
+      </FormItem>
+    </Form>
+  </ConfigProvider>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import { Message } from 'vexip-ui'
+
+import type { Form } from 'vexip-ui'
+
+const providedProps = {
+  default: { clearable: true },
+  formItem: { required: true }
+}
+
+const form = ref<InstanceType<typeof Form>>()
+const formModel = reactive({})
+const loading = ref(false)
+
+const options = ['选项1', '选项2', '选项3']
+
+async function handleValidate() {
+  loading.value = true
+
+  const errors = await form.value!.validate()
+
+  if (errors.length) {
+    Message.error('表单验证不通过')
+  } else {
+    Message.success('表单验证不通过')
+  }
+
+  setTimeout(() => {
+    loading.value = false
+  }, 1000)
+}
+</script>
