ac33af5d5dad5e9252acb8878cfdbc5dfdf24462
qmhc <40221744+qmhc@users.noreply.github.com>
2023-01-12 10:16:39 +0800

    fix(input): plain-password button cannot click when clearable
    fix #229

--- docs/demos/input/password/demo.zh-CN.vue
+++ docs/demos/input/password/demo.zh-CN.vue
@@ -1,3 +1,17 @@
 <template>
   <Input type="password" plain-password style="max-width: 300px;"></Input>
+  <br />
+  <br />
+  <Input
+    type="password"
+    plain-password
+    clearable
+    style="max-width: 300px;"
+  ></Input>
 </template>
+
+<style scoped>
+.vxp-input {
+  max-width: 300px;
+}
+</style>
