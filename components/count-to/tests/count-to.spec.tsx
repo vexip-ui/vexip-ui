64aed4d04638592f8b32aa4b021c0a895a15f22b
qmhc <544022268@qq.com>
2023-01-16 15:33:57 +0800

    test: update base tests

--- components/count-to/tests/count-to.spec.tsx
+++ components/count-to/tests/count-to.spec.tsx
@@ -1,17 +1,44 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
+import { nextTick } from 'vue'
 import { mount } from '@vue/test-utils'
 import { CountTo } from '..'
-vi.useFakeTimers()
+
+import type { SpyInstance } from 'vitest'
 
 describe('CountTo', () => {
+  let timestamp = -16
+  let rafMock: SpyInstance | undefined
+
+  beforeEach(() => {
+    rafMock = vi.spyOn(window, 'requestAnimationFrame').mockImplementation(cb => {
+      timestamp += 16
+      cb(timestamp)
+      return timestamp
+    })
+  })
+
+  afterEach(() => {
+    rafMock?.mockRestore()
+
+    timestamp = -16
+    rafMock = undefined
+  })
+
   it('render', () => {
     const wrapper = mount(CountTo)
 
+    expect(wrapper.classes()).toContain('vxp-count-to')
     expect(wrapper.classes()).toContain('vxp-count-to-vars')
     expect(wrapper.find('span').text()).toEqual('0')
   })
 
   it('should autoplay when mounted', async () => {
-    const wrapper = mount(() => <CountTo start={0} end={100} duration={1000} appear={true} />)
+    const wrapper = mount(() => <CountTo start={0} end={100} />)
+
+    expect(wrapper.find('.vxp-count-to').text()).toEqual('0')
+
+    wrapper.findComponent(CountTo).vm.start()
+    await nextTick()
+    expect(wrapper.find('.vxp-count-to').text()).toEqual('100')
   })
 })
