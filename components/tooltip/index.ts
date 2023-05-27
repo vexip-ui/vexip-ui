59864aa7724de98b948aef5c22b4e0e9a1eb2e07
류한경 <hangyeong.ryu@gmail.com>
2023-05-27 20:18:41 +0900

    types(tooltip): incorrect TooltipTrigger type (#348)

--- components/tooltip/index.ts
+++ components/tooltip/index.ts
@@ -2,4 +2,4 @@ export { default as Tooltip } from './tooltip'
 export { tooltipProps } from './props'
 
 export type { TooltipProps, TooltipCProps } from './props'
-export type { TooltipTheme, ToopTipTrigger, TooltipVirtual, TooltipExposed } from './symbol'
+export type { TooltipTheme, TooltipTrigger, TooltipVirtual, TooltipExposed } from './symbol'
