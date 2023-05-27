5853e99b2b0c3b52121b56b414e46df66bbe3e88
류한경 <hangyeong.ryu@gmail.com>
2023-05-27 14:38:53 +0900

    types(timeline): incorrect TimelineItem type (#346)

--- components/timeline/props.ts
+++ components/timeline/props.ts
@@ -2,7 +2,7 @@ import { booleanProp, buildProps, eventProp } from '@vexip-ui/config'
 
 import type { ExtractPropTypes, PropType } from 'vue'
 import type { ConfigurableProps } from '@vexip-ui/config'
-import type { TimelinkItemType } from './symbol'
+import type { TimelineItemType } from './symbol'
 
 export const timelineProps = buildProps({
   pending: booleanProp,
@@ -19,7 +19,7 @@ export type TimelineProps = ExtractPropTypes<typeof timelineProps>
 export type TimelineCProps = ConfigurableProps<TimelineProps>
 
 export const timelineItemProps = buildProps({
-  type: String as PropType<TimelinkItemType>,
+  type: String as PropType<TimelineItemType>,
   color: String,
   label: [Number, String],
   dashed: booleanProp,
