322cb6e4a01790a6078dc611a16a7bedcbc173d1
qmhc <40221744+qmhc@users.noreply.github.com>
2023-06-17 16:00:23 +0800

    feat(renderer): support store variables and resue them via slot

---
+++ docs/demos/renderer/slot/demo.en-US.vue
@@ -0,0 +1,22 @@
+<template>
+  <Renderer v-slot="{ message }" :data="{ message: add(msg1, msg2) }">
+    Use many times:
+    <p>
+      {{ message }}
+    </p>
+    <p>
+      {{ message }}
+    </p>
+  </Renderer>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const msg1 = ref('Some content')
+const msg2 = ref('Other content')
+
+function add(left: string, right: string) {
+  return `${left} + ${right}`
+}
+</script>
