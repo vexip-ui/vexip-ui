b860f4c1756d3a8285c599abf7142a5b6cd25013
qmhc <544022268@qq.com>
2023-07-23 17:26:58 +0800

    feat(collapse): support use keyboard to control panel
    fix #393

--- components/collapse/symbol.ts
+++ components/collapse/symbol.ts
@@ -1,17 +1,20 @@
-import type { InjectionKey, Ref } from 'vue'
+import type { InjectionKey } from 'vue'
 
 export type CollapseArrowType = 'right' | 'left' | 'none'
 
 export interface PanelState {
-  expanded: Ref<boolean>,
+  tab?: HTMLElement | null,
+  label: string | number,
+  expanded: boolean,
   setExpanded: (expanded: boolean) => void
 }
 
 export interface CollapseState {
   arrowType: CollapseArrowType,
-  registerPanel(label: string | number, panel: PanelState): void,
-  unregisterPanel(label: string | number): void,
-  expandPanel(label: string | number, expanded: boolean): void
+  registerPanel(panel: PanelState): void,
+  unregisterPanel(panel: PanelState): void,
+  expandPanel(label: string | number, expanded: boolean): void,
+  refreshLabels: () => void
 }
 
 export const COLLAPSE_STATE: InjectionKey<CollapseState> = Symbol('COLLAPSE_STATE')
