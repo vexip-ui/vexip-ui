01829eb2345d6198ea4ff932e4f5ff58c3d963ee
sight <1453017105@qq.com>
2023-08-04 05:18:23 +0800

    fix(playground): fix download

--- playground/download/download.ts
+++ playground/download/download.ts
@@ -1,6 +1,6 @@
 import { saveAs } from 'file-saver'
 import index from './template/index.html?raw'
-import main from './template/main.ts?raw'
+import main from './template/main.js?raw'
 import pkg from './template/package.json'
 import config from './template/vite.config.js?raw'
 import readme from './template/README.md?raw'
@@ -24,23 +24,32 @@ export async function downloadProject(store: ReplStore) {
 
   // basic structure
   zip.file('index.html', index)
-  zip.file('package.json', JSON.stringify(pkg))
+  zip.file('package.json', JSON.stringify(pkg, null, 2))
   zip.file('vite.config.js', config)
   zip.file('README.md', readme)
 
   // project src
   const src = zip.folder('src')!
-  src.file('main.ts', main)
+  src.file('main.js', main)
 
   const files = store.getFiles()
+
   for (const file in files) {
-    if (file === 'import-map.json') {
+    const filename = stripSrcPrefix(file)
+    if (['import-map.json', 'main.ts'].includes(filename)) {
       continue
     }
-
-    src.file(file, files[file])
+    if (filename === 'tsconfig.json') {
+      zip.file(filename, files[file])
+    } else {
+      src.file(filename, files[file])
+    }
   }
 
   const blob = await zip.generateAsync({ type: 'blob' })
   saveAs(blob, 'vexip-project.zip')
+}
+
+function stripSrcPrefix(file: string) {
+  return file.replace(/^src\//, '')
 }
