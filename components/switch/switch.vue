d7287747ebe5ac68daa4774c1808add9b4c9958d
一只前端汪 <985313519@qq.com>
2023-08-11 11:12:01 +0800

    fix: click event triggered twice if hidden input internal (#400)

--- components/switch/switch.vue
+++ components/switch/switch.vue
@@ -14,6 +14,7 @@
       :disabled="isDisabled"
       @submit.prevent
       @change="handleChange()"
+      @click.stop
     />
     <span :class="nh.be('placeholder')">
       <span :class="nh.be('open-text')">
