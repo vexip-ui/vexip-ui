085cb407b7806694fb27f3fd0a17750fb78ce1b6
qmhc <40221744+qmhc@users.noreply.github.com>
2023-08-14 16:20:04 +0800

    feat(bubble): add type prop

---
+++ docs/demos/bubble/types/demo.zh-CN.vue
@@ -0,0 +1,11 @@
+<template>
+  <Space vertical>
+    <Bubble v-for="bubbleType in types" :key="bubbleType" :type="bubbleType">
+      气泡的内容
+    </Bubble>
+  </Space>
+</template>
+
+<script setup lang="ts">
+const types = ['default', 'primary', 'info', 'success', 'warning', 'error'] as const
+</script>
