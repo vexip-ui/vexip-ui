994db7c4fa00375899b82bf566461b201073afbd
winches <329487092@qq.com>
2023-08-19 16:47:33 +0800

    chore: adjust

--- components/affix/handleLast.ts
+++ components/affix/handleLast.ts
@@ -1,11 +1,13 @@
 import { type ShallowRef } from 'vue'
 
-let lastScroller: HTMLElement | null | undefined = null
+let lastScroller: Window | HTMLElement | null | undefined = null
 
 export function handleLastScroller(target: ShallowRef<HTMLElement | undefined>) {
   if (lastScroller !== target.value) {
-    if (lastScroller) {
-      const targetAffix = lastScroller.querySelector('.vxp-affix--fixed') as HTMLElement
+    if (lastScroller && lastScroller !== window && lastScroller !== document.documentElement) {
+      const targetAffix = (lastScroller as HTMLElement).querySelector(
+        '.vxp-affix--fixed'
+      ) as HTMLElement
 
       if (targetAffix) {
         const classArr = targetAffix.classList
