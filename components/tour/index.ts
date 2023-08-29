05f3cc55ac73836fda188fc9d6eacce4edfe0f44
qmhc <40221744+qmhc@users.noreply.github.com>
2023-08-29 09:21:50 +0800

    chore: update create template to use setup-script

--- components/tour/index.ts
+++ components/tour/index.ts
@@ -3,6 +3,8 @@ import Tour from './tour.vue'
 import type { ComponentPublicInstance } from 'vue'
 
 export { Tour }
+export { tourProps } from './props'
+
 export type TourExposed = ComponentPublicInstance & InstanceType<typeof Tour>
 
 export type { TourProps, TourCProps } from './props'
