1b31613790bb470b786f0b3f5db877dd31c23152
qmhc <40221744+qmhc@users.noreply.github.com>
2023-11-29 14:53:24 +0800

    feat(loading): add transferTo method

--- components/loading/tests/loading.spec.tsx
+++ components/loading/tests/loading.spec.tsx
@@ -1,12 +1,31 @@
 import { describe, expect, it, vi } from 'vitest'
+import { getCurrentInstance, nextTick } from 'vue'
 import { mount } from '@vue/test-utils'
 
 import Loading from '../loading.vue'
+import { LoadingManager } from '..'
 
 vi.useFakeTimers()
 
 function nextFrame() {
   return new Promise(resolve => requestAnimationFrame(resolve))
+}
+
+function createLoading() {
+  const Loading = new LoadingManager()
+
+  mount({
+    setup() {
+      const instance = getCurrentInstance()
+      const app = instance?.appContext.app
+
+      app?.use(Loading)
+
+      return () => <div></div>
+    }
+  })
+
+  return Loading
 }
 
 describe('Loading', () => {
@@ -18,29 +37,35 @@ describe('Loading', () => {
   })
 
   it('loading', async () => {
-    const wrapper = mount(Loading)
+    const Loading = createLoading()
 
-    wrapper.vm.startLoading({ percent: 0 })
+    Loading.open({ percent: 0 })
     await nextFrame()
     await nextFrame()
     vi.runOnlyPendingTimers()
     await nextFrame()
-    expect(wrapper.find('.vxp-loading__filler').attributes('style')).toMatch(/translateX\(.+\)/)
-    expect(wrapper.find('.vxp-loading__filler').attributes('style')).toMatch(/scaleX\(.+\)/)
-    expect(wrapper.find('.vxp-loading__filler').attributes('style')).not.toContain('translateX(0%)')
-    expect(wrapper.find('.vxp-loading__filler').attributes('style')).not.toContain('scaleX(0)')
+    const wrapper = document.querySelector('.vxp-loading')!
+    expect(wrapper).toBeTruthy()
+    const filter = wrapper.querySelector('.vxp-loading__filler') as HTMLElement
+    expect(filter.style.cssText).toMatch(/translateX\(.+\)/)
+    expect(filter.style.cssText).toMatch(/scaleX\(.+\)/)
+    expect(filter.style.cssText).not.toContain('translateX(0%)')
+    expect(filter.style.cssText).not.toContain('scaleX(0)')
   })
 
   it('max percent', async () => {
-    const wrapper = mount(Loading)
+    const Loading = createLoading()
 
-    wrapper.vm.startLoading({ percent: 0, maxPercent: 50 })
+    Loading.open({ percent: 0, maxPercent: 50 })
     await nextFrame()
     await nextFrame()
     vi.runAllTimers()
     await nextFrame()
-    expect(wrapper.find('.vxp-loading__filler').attributes('style')).toContain('translateX(-25%)')
-    expect(wrapper.find('.vxp-loading__filler').attributes('style')).toContain('scaleX(0.5)')
+    const wrapper = document.querySelector('.vxp-loading')!
+    expect(wrapper).toBeTruthy()
+    const filter = wrapper.querySelector('.vxp-loading__filler') as HTMLElement
+    expect(filter.style.cssText).toContain('translateX(-25%)')
+    expect(filter.style.cssText).toContain('scaleX(0.5)')
   })
 
   it('position', async () => {
@@ -93,5 +118,14 @@ describe('Loading', () => {
         }
       })
     )
+  })
+
+  it('transferTo', async () => {
+    const Loading = createLoading()
+    const el = document.createElement('div')
+    Loading.transferTo(el)
+
+    await nextTick()
+    expect(el.querySelector('.vxp-loading')).toBeTruthy()
   })
 })
