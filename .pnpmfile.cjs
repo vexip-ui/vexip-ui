f971e0bdead0df2df569dc89f66dafb60e34e659
qmhc <544022268@qq.com>
2023-12-05 21:27:34 +0800

    chore: update gulp config

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -1,12 +1,17 @@
 function readPackage(pkg) {
-  if (pkg.dependencies.fsevents && pkg.dependencies.fsevents.match(/[\^~]?1./)) {
-    pkg.dependencies.fsevents = '^2.3.2'
-  } else if (pkg.dependencies.chokidar && pkg.dependencies.chokidar.match(/[\^~]?2./)) {
-    pkg.dependencies.chokidar = '^3.5.3'
-  } else if (pkg.dependencies.micromatch && pkg.dependencies.micromatch.match(/[\^~]?3./)) {
-    pkg.dependencies.micromatch = '^4.0.5'
-  } else if (pkg.dependencies.uuid && pkg.dependencies.uuid.match(/[\^~]?3./)) {
-    pkg.dependencies.uuid = '^7.0.3'
+  /** @type Record<string, string> */
+  const deps = pkg.dependencies
+
+  if (deps.fsevents && deps.fsevents.match(/[\^~]?1./)) {
+    deps.fsevents = '^2.3.2'
+  } else if (deps.chokidar && deps.chokidar.match(/[\^~]?2./)) {
+    deps.chokidar = '^3.5.3'
+  } else if (deps.micromatch && deps.micromatch.match(/[\^~]?3./)) {
+    deps.micromatch = '^4.0.5'
+  } else if (deps.uuid && deps.uuid.match(/[\^~]?3./)) {
+    deps.uuid = '^7.0.3'
+  } else if (deps.loupe && deps.loupe.match(/[\^~]?2\.3\.[0-6]/)) {
+    deps.loupe = '^2.3.7'
   }
 
   return pkg
