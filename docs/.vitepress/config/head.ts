06498db78ea2206ef42780ae64e5bfe36954ca89
qmhc <40221744+qmhc@users.noreply.github.com>
2024-01-18 15:12:51 +0800

    docs: add baidu stat script

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -1,5 +1,7 @@
 import type { HeadConfig } from 'vitepress'
 
+const HOST_CHECK = 'if (!location.host.includes(\'vexipui\')) return;'
+
 const LANG_SCRIPT = `
 (() => {
   const lang = typeof navigator !== 'undefined'
@@ -12,7 +14,7 @@ const LANG_SCRIPT = `
 `
 
 const GOOGLE_MANAGER = `
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+(function(w,d,s,l,i){${HOST_CHECK}w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
@@ -23,8 +25,18 @@ const GOOGLE_GTAG = `
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-
 gtag('config', 'G-YDXQW4BVVX');
+`
+
+const BAIDU_STAT = `
+window._hmt = window._hmt || [];
+(function() {
+  ${HOST_CHECK}
+  const hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1a35ae01b3648fa8b858b3b221e73400";
+  const s = document.getElementsByTagName("script")[0]; 
+  s.parentNode ? s.parentNode.insertBefore(hm, s) : document.head.appendChild(hm);
+})();
 `
 
 export function getHeadConfig(): HeadConfig[] {
@@ -39,6 +51,7 @@ export function getHeadConfig(): HeadConfig[] {
     ['meta', { property: 'og:site_name', content: 'Vexip UI' }],
     ['meta', { property: 'og:url', content: 'https://www.vexipui.com/' }],
 
+    ['script', {}, BAIDU_STAT],
     ['script', {}, LANG_SCRIPT],
     ['script', {}, GOOGLE_MANAGER],
     ['script', {}, GOOGLE_GTAG],
