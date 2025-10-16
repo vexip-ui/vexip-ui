8a244e5c8f01d1663dc6bca4531478c8b9fd806a
gaoyi <gaoyia@qq.com>
2025-10-16 14:40:13 +0800

    feat(layout-fit): optimize advanced examples

--- docs/demos/layout-fit/basis/demo.zh-CN.vue
+++ docs/demos/layout-fit/basis/demo.zh-CN.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-resize="handleResize" class="demo-window-get-width" style="width: 600px; height: 600px">
+  <div v-resize="handleResize" class="demo-window-get-width" style="width: 300px; height: 600px">
     <div
       :style="{
         width: windowWidth + 'px',
@@ -14,19 +14,17 @@
         fit="contain"
         :is-scale="true"
       >
-        <div style=" height: 100%; overflow: auto; color: #000;background: #fff">
-          <h3>LayoutFit 示例</h3>
-          <h3>适合邪修开发H5</h3>
-          <p>这是一个自适应布局容器，会根据父容器尺寸自动调整内容大小。</p>
-          <p>这里可以是一个好看的H5页面</p>
-          <p>或根据设计稿自动生成的H5页面</p>
-          <p>使用绝对单位完成页面开发</p>
-
-          <p>窗口宽度: {{ windowWidth }}</p>
-          <p>缩放比例: {{ layoutFit?.scaleX }}|{{ layoutFit?.scaleY }}</p>
-          <p></p>
-          <p>实际的像素宽度：{{ layoutFit?.currentWidth }}</p>
-          <p>实际的像素高度：{{ layoutFit?.currentHeight }}</p>
+        <div
+          v-resize="handleH5Resize"
+          style="position: relative; overflow: auto; color: #000; background: #eee"
+        >
+          <!-- 导入的H5组件 -->
+          <H5Component
+            :window-width="windowWidth"
+            :scale="layoutFit?.scaleX"
+            :current-width="layoutFit?.currentWidth"
+            :current-height="layoutFit?.currentHeight"
+          ></H5Component>
         </div>
       </LayoutFit>
     </div>
@@ -36,18 +34,25 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import { LayoutFit } from 'vexip-ui'
+import H5Component from './h5-component.vue'
 const layoutFit = ref()
 // 窗口宽度
 const windowWidth = ref(0)
+
 // 设计稿宽度
 const designWidth = 300
-// 设计稿高度
-const designHeight = 900
+// 页面高度
+const designHeight = ref(0)
+// 监听页面高度变化
+function handleH5Resize(entry: ResizeObserverEntry) {
+  const { height } = entry.contentRect
+  designHeight.value = height
+}
 // H5页面高度
 const h5Height = computed(() => {
-  return (designHeight / designWidth) * windowWidth.value
+  return (designHeight.value / designWidth) * windowWidth.value
 })
-
+// 监听窗口变化
 function handleResize(entry: ResizeObserverEntry) {
   windowWidth.value = entry.contentRect.width
 }
@@ -65,5 +70,10 @@ function handleResize(entry: ResizeObserverEntry) {
   color: #000;
   resize: both;
   background-color: var(--vxp-color-primary-base);
+  scrollbar-width: none;
+}
+
+.demo-window-get-width::-webkit-scrollbar {
+  display: none;
 }
 </style>
