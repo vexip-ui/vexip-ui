29947ab941d4159a312fa2319e0f6086224d0d0f
qmhc <rocco@moego.pet>
2026-04-30 15:31:37 +0800

    fix(scripts): publish with npm instead of pnpm

--- common/scripts/src/publish.ts
+++ common/scripts/src/publish.ts
@@ -5,7 +5,6 @@ import { getPkgInfo, logger, run } from './utils'
 export interface PublishOptions {
   pkgDir: string,
   isDryRun?: boolean,
-  provenance?: boolean,
   releaseTag?: string,
 }
 
@@ -21,19 +20,10 @@ export async function publish(options: PublishOptions) {
 
   await writeFile(pkgPath, JSON.stringify(copiedPkg, null, 2), 'utf-8')
 
-  const useNpm = !!options.provenance
   const publishArgs = ['publish', '--access', 'public', '--registry', 'https://registry.npmjs.org/']
-
-  if (!useNpm) {
-    publishArgs.push('--no-git-checks')
-  }
 
   if (options.isDryRun) {
     publishArgs.push('--dry-run')
-  }
-
-  if (options.provenance) {
-    publishArgs.push('--provenance')
   }
 
   if (options.releaseTag) {
@@ -41,7 +31,7 @@ export async function publish(options: PublishOptions) {
   }
 
   try {
-    await run(useNpm ? 'npm' : 'pnpm', publishArgs, { stdio: 'pipe', cwd: pkgDir })
+    await run('npm', publishArgs, { stdio: 'pipe', cwd: pkgDir })
     logger.successText(`Successfully published v${pkg.version}`)
   } catch (error) {
     if ((error as any).stderr?.match(/previously published/)) {
